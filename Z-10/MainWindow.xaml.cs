0722d3b995b1a440c7f332fdb0febae20507f011
MichalStajerski <m918560@gmail.com>
2020-05-22 00:13:53 +0200

    dodano Lab10

---
+++ Z-10/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Z_10
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student SimpleStudent = new Student()
+        {
+            Id = 0,
+            Name = "Jan",
+            LastName = "Kowalski",
+            EnlistingDate = new DateTime(2015,10,3)
+
+        };
+
+        List<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 0,
+                Name = "Jan",
+                LastName = "Kowalski",
+                EnlistingDate = new DateTime(2015,10,3)
+            },
+            new Student()
+            {
+                Id = 1,
+                Name = "Jan",
+                LastName = "Nowak",
+                EnlistingDate = new DateTime(2016,10,3)
+            },
+            new Student()
+            {
+                Id = 2,
+                Name = "Jan",
+                LastName = "Kwiatkowski",
+                EnlistingDate = new DateTime(2017,10,3)
+            },
+            new Student()
+            {
+                Id = 3,
+                Name = "Jan",
+                LastName = "Smith",
+                EnlistingDate = new DateTime(2018,10,3)
+            }
+
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Single.DataContext = SimpleStudent;
+            
+            t2.SetBinding(TextBox.TextProperty, new Binding("Name"));
+
+            StudentLV.ItemsSource = students;
+            StudentLB.ItemsSource = students;
+        }
+
+      
+    }
+}
---
+++ Z-10/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z_10 
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FullName => Name + " " + LastName;
+      
+        public  DateTime EnlistingDate { get; set; }
+
+        public override string ToString()
+        {
+            return "[STUDENT]";
+        }
+
+
+    }
+}
